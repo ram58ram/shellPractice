df54bd3d6cf838fe0e6418d2ac96cd5e571c8fe3
ram58ram <ram58ram@gmail.com>
2023-08-29 16:53:34 -0400

    commit1

--- shell1.sh
+++ shell1.sh
@@ -9,12 +9,20 @@
 # fi
 # echo "end of the script"
 
-USERID=$(id -u)
-if [ $USERID -ne 0 ]; then
-    echo "this is not root user"
-    sudo su -
-    echo "switched to root success? $?"    
-else
-    echo "you are root user"
-fi
-yum install mysql -y
+# USERID=$(id -u)
+# if [ $USERID -ne 0 ]; then
+#     echo "this is not root user"
+#     sudo su -
+#     echo "switched to root success? $?"    
+# else
+#     echo "you are root user"
+# fi
+# yum install mysql -y
+
+fruitArray=("organge" "banna" "apple")
+dayArray=("MON", "SUN", "TUE")
+
+echo "fruits are $fruitArray"
+echo "fruits are ${fruitArray}"
+echo "fruits are ${dayArray[$@]}"
+echo "fruits are ${dayArray[$#]}"
