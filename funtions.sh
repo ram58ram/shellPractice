4e9527fbea921843a21400f2ad147fe3ce750fe7
ram58ram <ram58ram@gmail.com>
2023-08-29 17:23:14 -0400

    commit1

--- funtions.sh
+++ funtions.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 
 #DATE=$(date+%F-%H-%M-%S)
-DATE=$(date+%F)
+DATE=$(date +%F)
 SCRIPT_NAME=$0
 LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
 USERID=$(id -u)
 
 VALIDATE(){
 
-if [ $1 -ne 0]; then
+if [ $1 -ne 0 ]; then
     echo "$2 is a....FAILURE"
     exit 1
 else
@@ -16,7 +16,7 @@ else
 fi
 }
 
-if [ $USERID -ne 0]; then
+if [ $USERID -ne 0 ]; then
     echo "ERROR: Please run this script with root user"
     exit 1
 else
