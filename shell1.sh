2d89d6102c0eb12f391873f1878185e9b0b6d810
ram58ram <ram58ram@gmail.com>
2023-08-29 16:17:46 -0400

    commit1

--- shell1.sh
+++ shell1.sh
@@ -6,5 +6,5 @@ PERSON2="Suresh"
 
 echo "Hello ${PERSON1}"
 echo "there $PERSON2"
-echo "this script executed at $(date +%M-%H-%m)"
-echo "this script executed at $(date +%M-%H-%m)"
+echo "this script executed at $(date +%M)"
+echo "this script executed at $(date)"
