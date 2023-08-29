9988469b4157278123727fd6835767c0ed7d0b84
ram58ram <ram58ram@gmail.com>
2023-08-29 16:26:25 -0400

    commit1

--- shell1.sh
+++ shell1.sh
@@ -8,3 +8,16 @@ echo "Hello ${PERSON1}"
 echo "there $PERSON2"
 echo "this script executed at $(date +%M)"
 echo "this script executed at $(date)"
+read "Enter your name"NAME
+echo "your name is $NAME"
+read -s "ENTER password"PASS
+echo "your PASSWORD is $PASS"
+read -n "is this in same line" line
+echo "yes same line $line"
+readh -s -n "is securre same line" linesecure
+echo "yes this is secure same line $linesecure"
+echo "command line 1 $1"
+echo "command line 2 $2"
+echo "command all lines $@"
+echo "command lines are @#"
+echo "command lines sing string are @*"
