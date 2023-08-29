39d57791ff16a14f9452da8d73713c568aa13476
ram58ram <ram58ram@gmail.com>
2023-08-29 16:44:20 -0400

    commit1

--- shell1.sh
+++ shell1.sh
@@ -1,23 +1,10 @@
 #! /bin/bash
 
-echo "Hello there"
-PERSON1="Ramesh"
-PERSON2="Suresh"
+NUUMBER=$1
 
-echo "Hello ${PERSON1}"
-echo "there $PERSON2"
-echo "this script executed at $(date +%M)"
-echo "this script executed at $(date)"
-read NAME
-echo "your name is $NAME"
-read -s PASS
-echo "your PASSWORD is $PASS"
-read -p line
-echo "yes same line $line"
-readh -p -s linesecure
-echo "yes this is secure same line $linesecure"
-echo "command line 1 $1"
-echo "command line 2 $2"
-echo "command all lines $@"
-echo "command lines are $#"
-echo "command lines sing string are $*"
+if [ $1 -ge 10 ]; then
+    echo "given number is greater than 10"
+else
+    echo "it is less than 10"
+fi
+echo "end of the script"
