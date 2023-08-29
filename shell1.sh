b44feeacdb4117992af30f1d584929b1f4702b5f
ram58ram <ram58ram@gmail.com>
2023-08-29 16:47:53 -0400

    commit1

--- shell1.sh
+++ shell1.sh
@@ -1,10 +1,18 @@
 #! /bin/bash
 
-NUUMBER=$1
+# NUUMBER=$1
 
-if [ $1 -ge 10 ]; then
-    echo "given number is greater than 10"
+# if [ $1 -ge 10 ]; then
+#     echo "given number is greater than 10"
+# else
+#     echo "it is less than 10"
+# fi
+# echo "end of the script"
+
+USERID=$(id -u)
+if [ USERID -ne 0 ]; then
+    echo "this is not root user"
 else
-    echo "it is less than 10"
+    sudo su -
+    yum install mysql -y
 fi
-echo "end of the script"
